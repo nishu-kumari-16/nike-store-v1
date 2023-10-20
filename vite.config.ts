637bebbef667da1c8a3bf3164ef7ae2991db5286
nishu-kumari-16 <nishu.kumari@codias.com>
2023-10-20 12:36:53 +0530

    add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	base: "/Nike-Store-v1",
+  plugins: [react()],
+  base: "/nike-store-v1",
 });
