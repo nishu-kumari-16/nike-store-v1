6dcebe7a9d5950399bebfa330a07f166d2f36a66
nishu-kumari-16 <nishu.kumari@codias.com>
2023-10-23 17:02:48 +0530

    fix regarding image issue

--- src/components/product-card/ProductCard.tsx
+++ src/components/product-card/ProductCard.tsx
@@ -12,34 +12,34 @@ export const ProductCard = ({
   price,
   discount,
   onClick,
-}: ProductCardProps) => (
-  <div
-    onClick={onClick}
-    className="cursor-pointer group hover:scale-105 transition-all w-80  bg-white shadow-lg rounded-lg"
-  >
-    <img
-      src={`${
-        import.meta.env.DEV
-          ? "../../public/"
-          : "https://abdulrahilsheikh.github.io/Nike-Store-v1/assets/"
-      }${img}`}
-      alt="adidas"
-      className="w-full rounded p-0 m-auto h-80 object-cover"
-    />
-    <div className=" p-2">
-      <div className=" p-2   bg-pink-200/50 rounded-lg bottom-0">
-        <p className="text-xl font-bold text-slate-600 ">{name}</p>
+}: ProductCardProps) => {
+  console.log(img);
 
-        <div className="flex items-center  w-full gap-4 mt-2">
-          <p className="text-gray-600 font-semibold">₹{price}</p>
-          <p className=" line-through text-gray-600  font-semibold">
-            ₹{Math.round((100 / (100 - discount)) * price)}
-          </p>
-          <p className="ml-auto font-semibold text-[#168aad] text-base">
-            {discount}% off
-          </p>
+  return (
+    <div
+      onClick={onClick}
+      className="cursor-pointer group hover:scale-105 transition-all w-80  bg-white shadow-lg rounded-lg"
+    >
+      <img
+        src={`${"../../public/"}${img}`}
+        alt="adidas"
+        className="w-full rounded p-0 m-auto h-80 object-cover"
+      />
+      <div className=" p-2">
+        <div className=" p-2   bg-pink-200/50 rounded-lg bottom-0">
+          <p className="text-xl font-bold text-slate-600 ">{name}</p>
+
+          <div className="flex items-center  w-full gap-4 mt-2">
+            <p className="text-gray-600 font-semibold">₹{price}</p>
+            <p className=" line-through text-gray-600  font-semibold">
+              ₹{Math.round((100 / (100 - discount)) * price)}
+            </p>
+            <p className="ml-auto font-semibold text-[#168aad] text-base">
+              {discount}% off
+            </p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
